715ef9a77c529d066ee3a9bde1396f6516968399
DerrickAmoateng <99464972+DerrickAmoateng@users.noreply.github.com>
2022-03-17 14:46:26 +0000

    adding files to repository

---
+++ public/main.js
@@ -0,0 +1,93 @@
+// Foursquare API Info
+const foursquareKey = 'fsq3gmghQQGxVCxuroBTBW/BRySu+DPJB4+GwSL6q9vMQXM=';
+const url = 'https://api.foursquare.com/v3/places/search?near=';
+
+// OpenWeather Info
+const openWeatherKey = 'b1981cf75fe18ce7f101caa3b04e7bdb';
+const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+// Page Elements
+const $input = $('#city');
+const $submit = $('#button');
+const $destination = $('#destination');
+const $container = $('.container');
+const $placeDivs = [$("#place1"), $("#place2"), $("#place3"), $("#place4"), $("#place5"), $("#place6")];
+const $weatherDiv = $("#weather1");
+const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+const options = {
+    method: 'GET',
+    headers: {
+        Accept: 'application/json',
+        Authorization: foursquareKey,
+    }
+};
+
+// Add AJAX functions here:
+const getPlaces = async() => {
+    const city = $input.val();
+    const urlToFetch = `${url}${city}&limit=10`;
+    try {
+        const response = await fetch(urlToFetch, options);
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            const places = [];
+            for (let result in jsonResponse.results) {
+                const randomIndex = Math.floor(Math.random() * jsonResponse.results.length);
+                if (places.indexOf(jsonResponse.results[randomIndex]) > -1) {
+                    continue;
+                } else {
+                    places.push(jsonResponse.results[randomIndex]);
+                }
+            }
+            return places;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const getForecast = async() => {
+    const urlToFetch = `${weatherUrl}?q=${$input.val()}&APPID=${openWeatherKey}`;
+    try {
+        const response = await fetch(urlToFetch);
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            return jsonResponse;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+// Render functions
+const renderPlaces = (places) => {
+    $placeDivs.forEach(($place, index) => {
+        // Add your code here:
+        const place = places[index];
+        const placeIcon = place.categories[0].icon;
+        const placeImgSrc = `${placeIcon.prefix}bg_64${placeIcon.suffix}`;
+
+        const placeContent = createPlaceHTML(place.name, place.location, placeImgSrc);
+        $place.append(placeContent);
+    });
+    $destination.append(`<h2>${places[0].location.locality}</h2>`);
+};
+
+const renderForecast = (forecast) => {
+    const weatherContent = createWeatherHTML(forecast);
+    $weatherDiv.append(weatherContent);
+};
+
+const executeSearch = () => {
+    $placeDivs.forEach(place => place.empty());
+    $weatherDiv.empty();
+    $destination.empty();
+    $container.css("visibility", "visible");
+    getPlaces().then(places => renderPlaces(places));
+    getForecast().then(forecast => renderForecast(forecast));
+    return false;
+}
+
+$submit.click(executeSearch);
